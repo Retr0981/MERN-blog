99a97bea44e58a53fe7b420e7d875912bf224a70
Retr0981 <davidarmah991@gmail.com>
2024-10-09 11:53:52 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,38 +1,68 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    // Validate the request body
-    if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return next(errorHandler(400, 'All fields are required'));
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
     }
-
-    try {
-        // Ensure that the password is a string
-        if (typeof password !== 'string') {
-            return next(errorHandler(400, 'Password must be a string'));
-        }
-
-        // Hash the password using bcryptjs
-        const hashedPassword = bcryptjs.hashSync(password, 10); // 10 is the salt rounds
-
-        // Create a new user instance
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        // Save the user to the database
-        await newUser.save();
-
-        // Send success response
-        res.json({ message: 'Signup Successful' });
-    } catch (error) {
-        // Handle any errors that occur during the save operation
-        next(error);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
     }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
